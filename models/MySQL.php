1abe72feedffe62ba75935503461e2d8c557c69b
Sleider <sleider@osiris.summer>
2020-03-17 22:30:53 +0100

    Añadida la clase wrapper para mysql y una lista de tareas

---
+++ models/MySQL.php
@@ -0,0 +1,63 @@
+<?php
+
+  class MySQL {
+
+    private $conexion; // Here is saved the MySQL conexion
+
+    function __construct() {}
+
+    function connect( $mysql_connector ) {
+      // This function sets the conection. A mysql connector is required thru parameter.
+
+      if (!$mysql_connector instanceof MySQLConnector) {
+        // The input is not a MySQL Connector
+        return false;
+      }
+
+      try {
+        $this->conexion = new PDO(
+          $mysql_connector->getDSN(),
+          $mysql_connector->getUser(),
+          $mysql_connector->getPass(),
+          $mysql_connector->options
+        );
+
+        return true;
+
+      } catch ( PDOException $err ) {
+        // An error with the DB conexion has occur. We may want to log this into a file.
+        return false;
+      }
+    }
+
+    // MAIN METHODS
+
+    function get () {}
+
+    function put () {}
+
+    function update () {}
+
+    function remove () {}
+
+
+    // UTILITY METHODS
+    function prepare( $query ) {
+      // Probably we don't need this.
+    }
+    function execute() {
+      // Executes a query
+    }
+
+    function setCondition() {}
+
+    function setSelect () {}
+
+    function setTable () {}
+
+    function setOrderBy () {}
+
+
+
+  }
+?>
